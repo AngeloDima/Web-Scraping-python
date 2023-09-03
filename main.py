b6f3e7dc65dd8505b7a249b41bae39ccee291edc
AngeloDima <angelodimauro91@gmail.com>
2023-09-03 16:46:07 +0200

    metto un limite di risultati

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ url = input("Inserisci il sito: ")  # Sito target
 res = requests.get(url)
 
 soup = bs4.BeautifulSoup(res.content, "html.parser")
-results = soup.findAll(
-    "div", class_="SmallCard-module_item-key-data__fcbjY"
-)  # Correzione qui
+results = soup.findAll("div", class_="SmallCard-module_item-key-data__fcbjY")
+
+maxCount = 3
 
 if results:
     with open("testo.txt", "a") as file:
+        count = 0
         for result in results:
-            file.write(result.text + "\n")  # Scrivi il testo su una nuova riga
+            file.write(result.text + "\n" + "\n")  # Scrivi il testo su una nuova riga
+            # Limite risultati
+            count += 1
+            if count >= maxCount:
+                break
+
+
 else:
     print("Elemento non trovato")
